bbeb10bc181ed056b7bb23bc44caff409928b4fd
233518 <60347694+233518@users.noreply.github.com>
2020-03-05 23:31:01 +0000

    Update config.php

--- php/config.php
+++ php/config.php
@@ -4,7 +4,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', '192.168.0.2'); //Database IP
 define('DB_USERNAME', 'root'); //Database username
 define('DB_PASSWORD', 'EAYyMZUJJrB3A9'); //Database password
-define('DB_NAME', 'PRO1000'); //Database name
+define('DB_NAME', 'pro1000'); //Database name
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
