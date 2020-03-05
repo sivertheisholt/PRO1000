e0a0fba9939427e81d31cd1f8bdf3edcfe2bc449
233518 <60347694+233518@users.noreply.github.com>
2020-03-05 23:19:11 +0000

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'PRO1000');
+define('DB_SERVER', '192.168.0.2'); //Database IP
+define('DB_USERNAME', 'root'); //Database username
+define('DB_PASSWORD', 'EAYyMZUJJrB3A9'); //Database password
+define('DB_NAME', 'PRO1000'); //Database name
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
