cc1a40b6ba3673d866465f20c56c43601ad87844
dyb0 <50889290+dyb0@users.noreply.github.com>
2020-03-14 11:42:22 +0100

    new sql for users

---
+++ php/accountpage/users.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(100)
+);
