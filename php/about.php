19c95fc56dd52e0e05f3a701f02f33febd1bd7c6
233518 <233518@usn.no>
2020-03-22 20:20:01 +0000

    Added about page

---
+++ php/about.php
@@ -0,0 +1,47 @@
+<?php
+// Initialize the session
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About-us Page</title>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!--CSS Links-->
+	<link rel="stylesheet" href="../css/mobile/about_mobile.css">
+	<link rel="stylesheet" type="text/css" href="../css/mobile/nav_mobile.css">
+</head>
+<body>
+	<div class="container">
+		<div class="about">
+			<div class="text">
+				<h1>About Us</h1>
+				<div class="line"></div>
+			</div>
+		</div>
+		<div class="content">
+			<p><b>L</b>orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.orem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+		</div>
+		<!-- Navigation bar -->
+        <div class="navbar">
+            <a href="../php/storymap.php">Home</a>
+            <a href="../php/attractions.php">Attractions</a>
+            <a class ="active" href="../php/about.php">About</a>
+            <a href="../php/accountpage.php">Account</a>
+        </div>
+		<footer>Copyright &copy;|About us|</footer>
+</body>
+</html>
