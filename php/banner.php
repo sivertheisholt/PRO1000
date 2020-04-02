639e9c18bc8b7636127b80ba8d91d6ee2799e6c7
dyb0 <dybevik2@gmail.com>
2020-04-02 22:07:26 +0200

    banner for desktop

---
+++ php/banner.php
@@ -0,0 +1,36 @@
+<DOCTYPE html>
+<html lang="en">
+<head>
+      <meta chaset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="../css/desktop/banner_desktop.css">
+      <title>Banner</title>
+</head>
+<body>
+  <div class="nav_wrapper">
+    <div class="menu-wrap">
+    <input type="checkbox" class="toggler">
+    <div class="hamburger"><div></div></div>
+    <div class="menu">
+      <div>
+        <div>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Attractions</a></li>
+            <li><a href="#">Trips</a></li>
+            <li><a href="#">Login</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <ul class="nav">
+      <div class="logo">
+        <img src="../storage/mobile/storymapbannertransparent.png">
+        <a class="logo_text">Enjoy a storymap of Barcelona's most beautiful places</a>
+      </div>
+  </ul>
+</div>
+</body>
+</html>           
